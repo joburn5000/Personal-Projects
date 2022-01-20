2a52e19a0d928062a7e2bf282c3e713222592659
joburn5000 <86541546+joburn5000@users.noreply.github.com>
2022-01-20 17:44:05 -0600

    Update 3Sum.py

--- 3Sum.py
+++ 3Sum.py
@@ -18,13 +18,11 @@ class Solution(object):
         # brute force:
         
         if len(nums) <3: return []
-        
         result = []
-        
         for i in range(len(nums)-2):
             for j in range(1,len(nums)-1):
                 for k in range(2, len(nums)):
-                    if ((nums[i]+nums[j]+nums[k]) == 0) & (i != j) & (i != k) & (j != k):
+                    if ((nums[i]+nums[j]+nums[k]) == 0):
                         array = [nums[i],nums[j],nums[k]]
                         array.sort()
                         if not array in result:
