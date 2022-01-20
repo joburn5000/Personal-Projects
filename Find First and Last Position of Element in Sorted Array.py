f96879beba9cf3438c03cb0a1d3328df88a920e4
joburn5000 <86541546+joburn5000@users.noreply.github.com>
2022-01-20 16:45:34 -0600

    Create Find First and Last Position of Element in Sorted Array.py

---
+++ Find First and Last Position of Element in Sorted Array.py
@@ -0,0 +1,39 @@
+"""
+If target is not found in the array, return [-1, -1].
+
+You must write an algorithm with O(log n) runtime complexity.
+
+
+Example 1:
+
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+Example 2:
+
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+Example 3:
+
+Input: nums = [], target = 0
+Output: [-1,-1]
+
+Completed: 1/20/2022
+"""
+
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        first = -1
+        last = -1
+        for index in range(len(nums)):
+            if nums[index] == target:
+                last = index
+                if first == -1:
+                    first = index
+        
+        return [first, last]
